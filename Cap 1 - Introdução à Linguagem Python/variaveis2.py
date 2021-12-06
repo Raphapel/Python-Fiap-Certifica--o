8504c56480863e98702a5bd39be7639ae8a7318f
Raphapel <rapha18023@gmail.com>
2021-12-06 18:13:44 -0300

    first commit

---
+++ Cap 1 - Introdução à Linguagem Python/variaveis2.py
@@ -0,0 +1,6 @@
+nome = input("digite o nome: ")
+cliente = input("digite o nome do cliente: ")
+valorEntrada = float(input("informe o valor da entrada: "))
+evento = input("informe o evento que esteve: ")
+
+print("declaro para o(a) senhor(a) " +nome+ " que o senhor(a) " +cliente+ " esteve no evento " +evento+ " e gastou o valor de R$" +str(valorEntrada)+ " com a entrada.")
