cf1fe65b9495b27c34f30565d52430a79509c760
Raphapel <rapha18023@gmail.com>
2021-12-13 12:42:33 -0300

    master

---
+++ Cap 4 - Manipulação de Dicionário de dados e Tuplas/cap4_dicionarios/managerUsers.py
@@ -0,0 +1,14 @@
+from funcoes import *
+usuarios = {}
+
+opcao = perguntar()
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
+    if opcao == "I":
+        inserir(usuarios)
+    elif opcao == "P":
+        pesquisar(usuarios, input("Qual login deseja pesquisar? "))
+    elif opcao == "E":
+        excluir(usuarios, input("Qual login deseja excluir? "))
+    elif opcao == "L":
+        listar(usuarios)
+    opcao = perguntar()
